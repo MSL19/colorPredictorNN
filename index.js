a75d0a10664fa31d8a0d86acc068a02fe01e48fb
MSL19 <40368820+MSL19@users.noreply.github.com>
2018-09-16 00:43:40 -0600

    finihsed the NN color picker  predictor....lat least for now

--- index.js
+++ index.js
@@ -8,32 +8,68 @@ function pickColor(){
     r = random(255);
     g = random(255);
     b = random(255);
+    redraw();
 }
 function setup(){
     createCanvas(600,300);
-    pickColor();
+    noLoop();
+
     brain = new NeuralNetwork(3,3,2);
+    pickColor();
 }
 function mousePressed(){
+    let targets;
+    if(mouseX > width/2){
+        targets = [0,1];
+    }
+    else{
+        targets = [1,0];
+    }
+    let inputs = [r/255,g/255,b/255];
+    brain.train(inputs, targets);
     pickColor();
+    
+}
+
+function colorPredictor(r,g,b){
+   /* if((r+g+b)>300){
+        return "black";
+    }
+    else{
+        return "white";
+    }
+    //This is a human learning algorithm
+    */
+   let inputs = [r/255,g/255,b/255];
+   let outputs = brain.predict(inputs);
+   console.log(outputs);
+   if(outputs[0]>outputs[1]){
+       return "black";
+   }
+   else{
+       return "white";
+   }
 }
 function draw(){
     background(r,g,b);
-
+    strokeWeight(4);
+    stroke(0);
+    line(width/2,0,width/2,height);
     textSize(64);
     noStroke();
     fill(0);
     textAlign(CENTER,CENTER);
 
-    text("black", 200, 150);
+    text("black", 150, 150);
     fill(255);
-    text("white", 400,150 );
+    text("white", 450,150 );
+    let which = colorPredictor(r,g,b);
     if(which === "black"){
         fill(0);
-        ellipse(200,250,60);
+        ellipse(150,250,60);
     }
     else{
         fill(255);
-        ellipse(400,250,60);
+        ellipse(450,250,60);
     }
 }
